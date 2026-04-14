f68738b60b3713c523f44e9db6406dae709a5a00
anguschoclab <59230472+anguschoclab@users.noreply.github.com>
2026-04-14 18:53:35 +0200

    updates

--- src/test/engine/bardEngine.test.ts
+++ src/test/engine/bardEngine.test.ts
@@ -3,6 +3,7 @@ import { SeededRNGService } from "@/engine/core/rng/SeededRNGService";
 import { NarrativeTemplateEngine } from "@/engine/narrative/narrativeTemplateEngine";
 import { blurb, commentatorFor, recapLine } from "@/lore/AnnouncerAI";
 import { TournamentSelectionService } from "@/engine/matchmaking/tournamentSelection";
+import { interpolateTemplate } from "@/engine/narrativePBP";
 
 describe("Bard Narrative Engine", () => {
   const rng = new SeededRNGService(12345);

38acdca3f7721f721388a01db4e1ad7cc67af430
anguschoclab <59230472+anguschoclab@users.noreply.github.com>
2026-04-14 14:16:10 +0200

    bun

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   test: {
     environment: 'jsdom',
     globals: true,
